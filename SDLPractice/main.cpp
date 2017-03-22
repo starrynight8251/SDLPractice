4cd910f0593d3e70ca4e1a0867b8a3b05c31034b
starrynight8251 <starrynight8251@gmail.com>
2017-03-22 21:15:38 +0900

    test commit

--- SDLPractice/main.cpp
+++ SDLPractice/main.cpp
@@ -1,15 +1,25 @@
-//
-//  main.cpp
-//  SDLPractice
-//
-//  Created by starrynight on 2017/03/22.
-//  Copyright © 2017年 NoCompany. All rights reserved.
-//
+#include <SDL2/SDL.h>
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int, char ** const)
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("Hey",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,640,480,0);
+    SDL_Renderer* render = SDL_CreateRenderer(window, -1, 0);
+    
+    
+    SDL_Event ev;
+    while(true){
+        SDL_SetRenderDrawColor(render, 0, 0, 0, 255);
+        SDL_RenderClear(render);
+        while(SDL_PollEvent(&ev))
+        {
+            if (ev.type == SDL_QUIT)
+                return 0;
+        }
+        SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
+        SDL_RenderDrawLine(render,10, 10, 400, 400);
+        SDL_RenderPresent(render);
+    }
+    
     return 0;
 }
