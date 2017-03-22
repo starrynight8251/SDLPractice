32009503731613f89144c8055b0fb2ad50f43243
starrynight8251 <starrynight8251@gmail.com>
2017-03-22 20:56:45 +0900

    Initial Commit

---
+++ SDLPractice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDLPractice
+//
+//  Created by starrynight on 2017/03/22.
+//  Copyright © 2017年 NoCompany. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
