30e77d37290ff01d92ea3a1155061a707b405c64
starrynight8251 <starrynight8251@gmail.com>
2017-04-20 22:48:58 +0900

    微修正

--- SDLPractice/particle.h
+++ SDLPractice/particle.h
@@ -12,23 +12,13 @@
 class Particle
 {
 public:
-    //Initialize position and animation
     Particle( int x, int y );
-    
-    //Shows the particle
     void render(SDL_Rect& camera);
-    
-    //Checks if particle is dead
     bool isDead();
     
 private:
-    //Offsets
     int mPosX, mPosY;
-    
-    //Current frame of animation
     int mFrame;
-    
-    //Type of particle
     LTexture *mTexture;
 };
 
